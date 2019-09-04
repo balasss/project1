8ab0c8043a1e1c652e44a8dfc91809254c5b23ae
balasss <47716534+balasss@users.noreply.github.com>
2019-09-04 19:05:36 +0530

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,8 @@
+ <div class="sidebar">
+ 
+  <a href="#"><p data-step="1" data-intro="Home" onclick="javascript:introJs().start();">Home!</p></a>
+  <a href="#"><p data-step="2" data-intro="About us">About us</p></a>
+  <a href="#"><p data-step="3" data-intro="Blogs">Blog </p></a>
+  <a href="#"><p data-step="4" data-intro="News">News</p></a>
+ <script type="text/javascript" src="http://usablica.github.io/intro.js/intro.js?v=060"></script>
+    </div><!-- .sidebar -->
