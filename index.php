673e7f420ed008f2591a4cf4d3b6483ae19522a7
balasss <47716534+balasss@users.noreply.github.com>
2019-09-04 18:55:38 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Demo Layout</title>
+<link type="text/css" rel="stylesheet" href="css/style.css">
+<link type="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="http://usablica.github.io/intro.js/introjs.css?v=060">
+</head>
+<body>
+ 
+<div class="wrap">
+ 
+ <?php  include "header.php"; 
+		include "sidebar.php";  
+		include "content.php";  
+		include "clear.php";  
+		include "footer.php"; ?>
+
+</div>
+ 
+</body>
+</html>
